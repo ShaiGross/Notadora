ac4341575a66a94c1cb27ec37d1522618b8f7160
ShaiGross <shaigros91@gmail.com>
2017-04-21 18:42:41 +0300

    Revert from this shit

--- DevelopmentDriver/DevResources/PrintMatches.sql
+++
@@ -1,8 +0,0 @@
-﻿SELECT CONCAT (v.Infinative, '( ', 
-			  v.EnglishInfinative, ')', ' applies rule ', 
-			  CR.Name, ' for person ', 
-			  P.SpanishExpression, ' with string ', CM.ConjugationString)
-FROM Verbs v
-JOIN ConjugationMatches CM ON v.Id = CM.VerbId
-JOIN ConjugationRules CR on CM.ConjugationRuleId = CR.Id
-JOIN Persons P on p.Id = CM.PersonId
--- README.md
+++
@@ -1,2 +0,0 @@
-# Notadora
-nothing to tell
