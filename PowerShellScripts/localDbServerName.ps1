900f7089502ebfdd9bcd919e8386613957898996
ShaiGross <shaigros91@gmail.com>
2017-08-09 01:16:50 +0300

    asdf

---
+++ PowerShellScripts/localDbServerName.ps1
@@ -0,0 +1,18 @@
+﻿
+SQLLOCALDB stop NotaDbServer
+while ($true) {
+
+    $info = SQLLOCALDB info NotaDbServer
+    
+    if (-not ($info -match "np")) {
+    
+        SQLLOCALDB start  NotaDbServer;
+        $info = SQLLOCALDB info NotaDbServer
+        $splitOutput = $info.Split(":");
+        $srvName = $splitOutput[$splitOutput.Count - 2];        
+
+        Write-Host "np:$srvName"
+    }
+
+    sleep 1;
+}
